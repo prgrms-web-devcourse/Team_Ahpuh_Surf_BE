a3437ef11caf6eb774e19fb58b1055420ccd04f3
cse0518 <g787@naver.com>
2021-12-09 14:55:33 +0900

    docs: yml 설정 수정, style 수정

--- src/main/java/org/ahpuh/surf/common/exception/EntityExceptionHandler.java
+++ src/main/java/org/ahpuh/surf/common/exception/EntityExceptionHandler.java
@@ -10,18 +10,12 @@ public class EntityExceptionHandler {
         return new IllegalArgumentException("Category with given id not found. Invalid id is " + categoryId);
     }
 
-    ;
-
     public static IllegalArgumentException UserNotFound(final Long userId) {
         return new IllegalArgumentException("User with given id not found. Invalid id is " + userId);
     }
 
-    ;
-
     public static IllegalArgumentException PostNotFound(final Long postId) {
         return new IllegalArgumentException("Post with given id not found. Invalid id is " + postId);
     }
-
-    ;
 
 }
