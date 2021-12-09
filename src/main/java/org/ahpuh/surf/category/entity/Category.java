0137d6a71ae6eaa6cda785d62a7d4afa7fa15b0a
cse0518 <g787@naver.com>
2021-12-09 15:10:02 +0900

    fix: user-category 연관관계 매핑

--- src/main/java/org/ahpuh/surf/category/entity/Category.java
+++ src/main/java/org/ahpuh/surf/category/entity/Category.java
@@ -4,8 +4,13 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.ahpuh.surf.aop.SoftDelete;
 import org.ahpuh.surf.common.entity.BaseEntity;
+import org.ahpuh.surf.post.entity.Post;
+import org.ahpuh.surf.user.entity.User;
+import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -33,22 +38,22 @@ public class Category extends BaseEntity {
     @Column(name = "average_score")
     private int averageScore;
 
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "user_id")
-//    private User user;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-//    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Post> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
 
-//    @Formula("(select count(1) from post where is_deleted = false)")
-//    private int postCount;
+    @Formula("(select count(1) from post where is_deleted = false)")
+    private int postCount;
 
     // Todo: user 양방향 관계 메소드 setUser(User user) {}
     //  post 양방향 관계 메소드 addPost(Post post) {}
 
     @Builder
-    public Category(final String name, final boolean isPublic, final int averageScore, final String colorCode) {
-//        this.user = user;
+    public Category(final User user, final String name, final boolean isPublic, final int averageScore, final String colorCode) {
+        this.user = user;
         this.name = name;
         this.isPublic = isPublic;
         this.colorCode = colorCode;
