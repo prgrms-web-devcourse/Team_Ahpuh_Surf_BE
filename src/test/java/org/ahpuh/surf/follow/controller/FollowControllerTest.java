7f226fa34a501b43ede9b1188ab19f3413eb6c87
cse0518 <g787@naver.com>
2021-12-11 22:40:57 +0900

    test: unfollow API test

--- src/test/java/org/ahpuh/surf/follow/controller/FollowControllerTest.java
+++ src/test/java/org/ahpuh/surf/follow/controller/FollowControllerTest.java
@@ -1,12 +1,14 @@
 package org.ahpuh.surf.follow.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ahpuh.surf.follow.repository.FollowRepository;
 import org.ahpuh.surf.user.controller.UserController;
 import org.ahpuh.surf.user.dto.UserLoginRequestDto;
 import org.ahpuh.surf.user.entity.User;
 import org.ahpuh.surf.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +33,8 @@ class FollowControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private FollowRepository followRepository;
     @Autowired
     private FollowController followController;
     @Autowired
@@ -70,9 +75,11 @@ class FollowControllerTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("팔로우를 할 수 있다.")
     @Transactional
     void testFollow() throws Exception {
+        // Given
         final User user1 = userRepository.getById(userId1);
         final User user2 = userRepository.getById(userId2);
 
@@ -83,6 +90,7 @@ class FollowControllerTest {
                 () -> assertThat(user2.getFollowingUsers().size(), is(0))
         );
 
+        // When
         mockMvc.perform(post("/api/v1/follow")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(userId2))
@@ -90,6 +98,7 @@ class FollowControllerTest {
                 .andExpect(status().isCreated())
                 .andDo(print());
 
+        // Then
         assertAll("afterFollow",
                 () -> assertThat(user1.getFollowedUsers().size(), is(1)),
                 () -> assertThat(user1.getFollowedUsers().get(0).getUser().getUserId(), is(userId1)),
@@ -97,6 +106,41 @@ class FollowControllerTest {
                 () -> assertThat(user2.getFollowingUsers().size(), is(1)),
                 () -> assertThat(user2.getFollowingUsers().get(0).getUser().getUserId(), is(userId1)),
                 () -> assertThat(user2.getFollowingUsers().get(0).getFollowedUser().getUserId(), is(userId2))
+        );
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("언팔로우를 할 수 있다.")
+    @Transactional
+    void testUnfollow() throws Exception {
+        // Given
+        mockMvc.perform(post("/api/v1/follow")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userId2))
+                        .header("token", token))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        assertAll("beforeFollow",
+                () -> assertThat(userRepository.getById(userId1).getFollowedUsers().size(), is(1)),
+                () -> assertThat(userRepository.getById(userId2).getFollowingUsers().size(), is(1)),
+                () -> assertThat(followRepository.findAll().size(), is(1))
+        );
+        final Long followid = followRepository.findAll().get(0).getFollowId();
+
+        // When
+        mockMvc.perform(delete("/api/v1/follow/{followId}", followid)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("token", token))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+
+        // Then
+        assertAll("afterFollow",
+                () -> assertThat(userRepository.getById(userId1).getFollowedUsers().size(), is(0)),
+                () -> assertThat(userRepository.getById(userId2).getFollowingUsers().size(), is(0)),
+                () -> assertThat(followRepository.findAll().size(), is(0))
         );
     }
 
