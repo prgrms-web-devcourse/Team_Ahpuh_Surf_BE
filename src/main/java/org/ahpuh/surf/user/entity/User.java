de34410c6bed34a589b3e3c9a7703d28541ae18b
cse0518 <g787@naver.com>
2021-12-12 00:00:52 +0900

    fix: 권한이 자동으로 설정되게 변경

--- src/main/java/org/ahpuh/surf/user/entity/User.java
+++ src/main/java/org/ahpuh/surf/user/entity/User.java
@@ -51,7 +51,8 @@ public class User extends BaseEntity {
     private Boolean accountPublic = true;
 
     @Column(name = "permission")
-    private String permission;
+    @Builder.Default
+    private String permission = "ROLE_USER";
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
