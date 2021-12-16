e7edd733e7018bf72f3a5867d2eb909a2c874424
cse0518 <g787@naver.com>
2021-12-16 21:06:42 +0900

    feat: post-like 연관관계 매핑

--- src/main/java/org/ahpuh/surf/like/repository/LikeRepository.java
+++ src/main/java/org/ahpuh/surf/like/repository/LikeRepository.java
@@ -1,12 +1,13 @@
 package org.ahpuh.surf.like.repository;
 
 import org.ahpuh.surf.like.entity.Like;
+import org.ahpuh.surf.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
-    Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
+    Optional<Like> findByUserIdAndPost(Long userId, Post post);
 
 }
