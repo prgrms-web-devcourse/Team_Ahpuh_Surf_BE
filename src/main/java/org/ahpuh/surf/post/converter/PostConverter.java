828bda3e1bde7ccb8673bf60e3993a3c405b3040
cse0518 <g787@naver.com>
2021-12-18 03:24:30 +0900

    refactor: S3Service 리팩토링

--- src/main/java/org/ahpuh/surf/post/converter/PostConverter.java
+++ src/main/java/org/ahpuh/surf/post/converter/PostConverter.java
@@ -3,7 +3,7 @@ package org.ahpuh.surf.post.converter;
 import org.ahpuh.surf.category.dto.CategorySimpleDto;
 import org.ahpuh.surf.category.entity.Category;
 import org.ahpuh.surf.common.exception.EntityExceptionHandler;
-import org.ahpuh.surf.common.s3.S3Service.FileStatus;
+import org.ahpuh.surf.common.s3.S3ServiceImpl.FileStatus;
 import org.ahpuh.surf.like.entity.Like;
 import org.ahpuh.surf.post.dto.*;
 import org.ahpuh.surf.post.entity.Post;
