608ea3a9bb614e3910b5d2f8731c160744e6ec27
SeungEun Choi/ 최승은 <g787@naver.com>
2022-02-20 19:42:51 +0900

    Flyway 도입 (#71)
    * chore: flyway 설정 추가
    
    * docs: flyway version1 init.sql 생성
    
    * docs: update readme
    
    * fix: flyway version 1 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,3 @@
-DROP TABLE IF EXISTS likes CASCADE;
-DROP TABLE IF EXISTS follow CASCADE;
-DROP TABLE IF EXISTS posts CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
-
 CREATE TABLE users
 (
     user_id           BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -13,11 +7,11 @@ CREATE TABLE users
     profile_photo_url TEXT,
     url               VARCHAR(255),
     about_me          VARCHAR(255),
-    account_public    BOOLEAN     DEFAULT true,
-    permission        VARCHAR(20) DEFAULT "ROLE_USER",
-    created_at        TIMESTAMP   DEFAULT current_timestamp,
-    updated_at        TIMESTAMP   DEFAULT current_timestamp,
-    is_deleted        BOOLEAN     DEFAULT false
+    account_public    BOOLEAN   DEFAULT true,
+    permission        VARCHAR(20),
+    created_at        TIMESTAMP DEFAULT current_timestamp,
+    updated_at        TIMESTAMP DEFAULT current_timestamp,
+    is_deleted        BOOLEAN   DEFAULT false
 );
 
 CREATE TABLE categories
