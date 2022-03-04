50a352ab1c742e4cba0fede44dea20684d95b5db
cse0518 <g787@naver.com>
2022-03-04 16:03:09 +0900

    test: Category Controller 테스트

--- src/test/java/org/ahpuh/surf/unit/ControllerTest.java
+++ src/test/java/org/ahpuh/surf/unit/ControllerTest.java
@@ -1,6 +1,8 @@
 package org.ahpuh.surf.unit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ahpuh.surf.category.controller.CategoryController;
+import org.ahpuh.surf.category.service.CategoryService;
 import org.ahpuh.surf.config.JwtConfig;
 import org.ahpuh.surf.user.controller.UserController;
 import org.ahpuh.surf.user.service.UserService;
@@ -15,11 +17,17 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureRestDocs
 @Import(JwtConfig.class)
 @ActiveProfiles("test")
-@WebMvcTest(UserController.class)
+@WebMvcTest({
+        UserController.class,
+        CategoryController.class
+})
 public abstract class ControllerTest {
 
     @MockBean
     protected UserService userService;
+
+    @MockBean
+    protected CategoryService categoryService;
 
     @Autowired
     protected MockMvc mockMvc;
