f9ec02a80c27b5dedfd513bcf897b868367d775b
SeungEun Choi/ 최승은 <g787@naver.com>
2022-03-04 15:59:05 +0900

    Category 도메인 예외 처리 (#95)
    * feat: CategoryNotFoundException 추가
    
    * refactor: 예외처리 적용 리팩토링
    
    * refactor: @Valid 예외처리 수정

--- src/main/java/org/ahpuh/surf/category/dto/request/CategoryCreateRequestDto.java
+++ src/main/java/org/ahpuh/surf/category/dto/request/CategoryCreateRequestDto.java
@@ -12,10 +12,11 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class CategoryCreateRequestDto {
 
-    @NotBlank(message = "Category name length must be 1 ~ 30.")
-    @Size(min = 1, max = 30)
+    @NotBlank(message = "Category name must not be blank.")
+    @Size(min = 1, max = 30, message = "Category name length must be 1 ~ 30.")
     private String name;
 
+    @NotBlank(message = "ColorCode must not be blank.")
     @Pattern(regexp = "^#(?:[0-9a-fA-F]{3}){1,2}$", message = "Invalid colorCode.")
     private String colorCode;
 
