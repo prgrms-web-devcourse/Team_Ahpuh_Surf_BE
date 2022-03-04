fd199ea118f06c461d49556404cabd2a40e8bb99
cse0518 <g787@naver.com>
2022-03-04 15:56:24 +0900

    refactor: @Valid 예외처리 수정

--- src/main/java/org/ahpuh/surf/category/dto/request/CategoryUpdateRequestDto.java
+++ src/main/java/org/ahpuh/surf/category/dto/request/CategoryUpdateRequestDto.java
@@ -13,13 +13,14 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class CategoryUpdateRequestDto {
 
-    @NotBlank(message = "Category name length must be 1 ~ 30.")
-    @Size(min = 1, max = 30)
+    @NotBlank(message = "Category name must not be blank.")
+    @Size(min = 1, max = 30, message = "Category name length must be 1 ~ 30.")
     private String name;
 
     @NotNull(message = "No is_public input.")
     private Boolean isPublic;
 
+    @NotBlank(message = "ColorCode must not be blank.")
     @Pattern(regexp = "^#(?:[0-9a-fA-F]{3}){1,2}$", message = "Invalid colorCode.")
     private String colorCode;
 
