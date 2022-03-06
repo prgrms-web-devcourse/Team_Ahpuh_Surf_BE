d91b1fa71fb0d557a631921dc3865e47d652cdbc
SeungEun Choi/ 최승은 <g787@naver.com>
2022-03-06 19:04:13 +0900

    local 환경 seed db 설정 (#114)
    * feat: seed 파일 추가
    
    * feat: flyway 설정 추가

---
+++ src/main/resources/db/seed/R__004_Seed_follow.sql
@@ -0,0 +1,9 @@
+INSERT IGNORE INTO `follow` (`follow_id`, `user_id`, `following_id`)
+VALUES
+    (1, 1, 7),
+    (2, 1, 2),
+    (3, 1, 3),
+    (4, 7, 1),
+    (5, 7, 2),
+    (6, 7, 6),
+    (7, 7, 8);
