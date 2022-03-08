ce13ecd26c291c30d1b5f048b0a97fdc915e6a1e
cse0518 <g787@naver.com>
2022-03-08 21:52:34 +0900

    refactor: GlobalExceptionHandler 예외 처리 추가

--- src/main/java/org/ahpuh/surf/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ahpuh/surf/common/exception/GlobalExceptionHandler.java
@@ -6,9 +6,13 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.net.BindException;
 
 import static java.util.Objects.requireNonNull;
 
@@ -42,6 +46,39 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse(errorMessage));
     }
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> bindException(
+            final BindException e
+    ) {
+        final String errorMessage = e.getMessage();
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), errorMessage);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentTypeMismatchException(
+            final MethodArgumentTypeMismatchException e
+    ) {
+        final String errorMessage = e.getMessage();
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), errorMessage);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedException(
+            final HttpRequestMethodNotSupportedException e
+    ) {
+        final String errorMessage = e.getMessage();
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), errorMessage);
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ErrorResponse(errorMessage));
+    }
+
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponse> applicationException(
             final ApplicationException e
@@ -54,7 +91,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<ErrorResponse> dataAccessException(final DataAccessException e) {
+    public ResponseEntity<ErrorResponse> dataAccessException(
+            final DataAccessException e
+    ) {
         final String errorMessage = e.getMessage();
         log.error(LOG_FORMAT, e.getClass().getSimpleName(), errorMessage);
         return ResponseEntity
@@ -63,7 +102,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> runtimeException(final RuntimeException e) {
+    public ResponseEntity<ErrorResponse> runtimeException(
+            final RuntimeException e
+    ) {
         log.error(LOG_FORMAT, e.getClass().getSimpleName(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
