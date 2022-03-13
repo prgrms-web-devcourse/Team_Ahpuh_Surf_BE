e7d80ac20058f38549eceb2f735f9063e9d21210
cse0518 <g787@naver.com>
2022-03-13 12:41:13 +0900

    refactor: IntegrationControllerTest 클래스 생성

---
+++ src/test/java/org/ahpuh/surf/integration/IntegrationControllerTest.java
@@ -0,0 +1,17 @@
+package org.ahpuh.surf.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+public abstract class IntegrationControllerTest extends IntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
