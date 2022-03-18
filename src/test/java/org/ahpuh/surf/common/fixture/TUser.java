d4a205bed3893913129b3bd85703e5706d54d4e3
cse0518 <g787@naver.com>
2022-03-18 21:49:25 +0900

    test: TUser 로직 추가

--- src/test/java/org/ahpuh/surf/common/fixture/TUser.java
+++ src/test/java/org/ahpuh/surf/common/fixture/TUser.java
@@ -12,17 +12,18 @@ import static org.ahpuh.surf.common.factory.MockUserFactory.*;
 import static org.ahpuh.surf.common.mapper.DtoObjectMapper.mapToString;
 
 public enum TUser {
-    USER_1("user1"),
-    USER_2("user2"),
-    USER_3("user3"),
-    USER_4("user4"),
-    USER_5("user5");
+    USER_1(1L, "user1"),
+    USER_2(2L, "user2"),
+    USER_3(3L, "user3"),
+    USER_4(4L, "user4"),
+    USER_5(5L, "user5");
 
+    public Long userId;
     public String email;
     public String userName;
     public ValidatableResponse response;
 
-    TUser(final String userName) {
+    TUser(final Long userId, final String userName) {
         this.userName = userName;
         this.email = userName + "@naver.com";
     }
@@ -43,13 +44,22 @@ public enum TUser {
                 .then();
     }
 
+    public void 회원가입_요청_name(final String userName) {
+        this.response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createUserJoinRequestDtoWithUserName(userName))
+                .request(Method.POST, "/api/v1/users")
+                .then();
+    }
+
     public void 회원가입_완료() {
         given().contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(createUserJoinRequestDtoWithEmail(email))
                 .request(Method.POST, "/api/v1/users");
     }
 
-    public void 회원가입_하지_않음() {}
+    public void 회원가입_하지_않음() {
+    }
 
     public void 로그인_요청() {
         this.response = given()
