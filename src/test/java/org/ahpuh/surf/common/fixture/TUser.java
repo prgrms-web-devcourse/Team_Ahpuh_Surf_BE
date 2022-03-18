3148ce245a715b43b3289e81116b85a645202e95
cse0518 <g787@naver.com>
2022-03-19 00:15:13 +0900

    refactor: User 인수테스트 수정

--- src/test/java/org/ahpuh/surf/common/fixture/TUser.java
+++ src/test/java/org/ahpuh/surf/common/fixture/TUser.java
@@ -2,6 +2,7 @@ package org.ahpuh.surf.common.fixture;
 
 import io.restassured.http.Method;
 import io.restassured.response.ValidatableResponse;
+import org.ahpuh.surf.user.dto.response.UserLoginResponseDto;
 import org.apache.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
@@ -21,9 +22,11 @@ public enum TUser {
     public Long userId;
     public String email;
     public String userName;
+    public String token;
     public ValidatableResponse response;
 
     TUser(final Long userId, final String userName) {
+        this.userId = userId;
         this.userName = userName;
         this.email = userName + "@naver.com";
     }
@@ -31,7 +34,7 @@ public enum TUser {
     public void 회원가입_요청() {
         this.response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserJoinRequestDtoWithEmail(email))
+                .body(createUserJoinRequestDtoWithEmail(this.email))
                 .request(Method.POST, "/api/v1/users")
                 .then();
     }
@@ -54,7 +57,7 @@ public enum TUser {
 
     public void 회원가입_완료() {
         given().contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserJoinRequestDtoWithEmail(email))
+                .body(createUserJoinRequestDtoWithEmail(this.email))
                 .request(Method.POST, "/api/v1/users");
     }
 
@@ -64,7 +67,7 @@ public enum TUser {
     public void 로그인_요청() {
         this.response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserLoginRequestDto(email))
+                .body(createUserLoginRequestDto(this.email))
                 .request(Method.POST, "/api/v1/users/login")
                 .then();
     }
@@ -72,21 +75,35 @@ public enum TUser {
     public void 로그인_요청(final String password) {
         this.response = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserLoginRequestDto(email, password))
+                .body(createUserLoginRequestDto(this.email, password))
                 .request(Method.POST, "/api/v1/users/login")
                 .then();
     }
 
-    public void 유저조회_요청(final String token, final Long userId) {
+    public AfterLoginAction 로그인_완료() {
+        given().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createUserJoinRequestDtoWithEmail(this.email))
+                .request(Method.POST, "/api/v1/users");
+        this.token = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createUserLoginRequestDto(this.email))
+                .request(Method.POST, "/api/v1/users/login")
+                .then().statusCode(200)
+                .extract().as(UserLoginResponseDto.class)
+                .getToken();
+        return new AfterLoginAction(this);
+    }
+
+    public void 유저조회_요청(final Long userId) {
         this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, token)
+                .header(HttpHeaders.AUTHORIZATION, this.token)
                 .request(Method.GET, "/api/v1/users/{userId}", userId)
                 .then();
     }
 
-    public void 유저정보_수정_요청_With_File(final String token, final File file) {
+    public void 유저정보_수정_요청_With_File(final File file) {
         this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, token)
+                .header(HttpHeaders.AUTHORIZATION, this.token)
                 .multiPart("file",
                         file,
                         MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -97,9 +114,9 @@ public enum TUser {
                 .then();
     }
 
-    public void 유저정보_수정_요청_No_File(final String token) {
+    public void 유저정보_수정_요청_No_File() {
         this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, token)
+                .header(HttpHeaders.AUTHORIZATION, this.token)
                 .multiPart("request",
                         mapToString(createUserUpdateRequestDto()),
                         MediaType.APPLICATION_JSON_VALUE)
@@ -107,9 +124,9 @@ public enum TUser {
                 .then();
     }
 
-    public void 회원탈퇴_요청(final String token) {
+    public void 회원탈퇴_요청() {
         this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, token)
+                .header(HttpHeaders.AUTHORIZATION, this.token)
                 .request(Method.DELETE, "/api/v1/users")
                 .then();
     }
