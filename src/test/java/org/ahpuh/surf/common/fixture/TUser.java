e50dd18498b44cb9421601b1fc9f1d156751abaf
cse0518 <g787@naver.com>
2022-03-22 15:35:06 +0900

    test: UserAction 클래스 생성

--- src/test/java/org/ahpuh/surf/common/fixture/TUser.java
+++ src/test/java/org/ahpuh/surf/common/fixture/TUser.java
@@ -3,14 +3,10 @@ package org.ahpuh.surf.common.fixture;
 import io.restassured.http.Method;
 import io.restassured.response.ValidatableResponse;
 import org.ahpuh.surf.user.dto.response.UserLoginResponseDto;
-import org.apache.http.HttpHeaders;
 import org.springframework.http.MediaType;
-
-import java.io.File;
 
 import static io.restassured.RestAssured.given;
 import static org.ahpuh.surf.common.factory.MockUserFactory.*;
-import static org.ahpuh.surf.common.mapper.DtoObjectMapper.mapToString;
 
 public enum TUser {
     USER_1(1L, "user1"),
@@ -81,53 +77,11 @@ public enum TUser {
     }
 
     public AfterLoginAction 로그인_완료() {
-        given().contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserJoinRequestDtoWithEmail(this.email))
-                .request(Method.POST, "/api/v1/users");
-        this.token = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(createUserLoginRequestDto(this.email))
-                .request(Method.POST, "/api/v1/users/login")
-                .then().statusCode(200)
+        회원가입_완료();
+        로그인_요청();
+        this.token = this.response.statusCode(200)
                 .extract().as(UserLoginResponseDto.class)
                 .getToken();
         return new AfterLoginAction(this);
-    }
-
-    public void 유저조회_요청(final Long userId) {
-        this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, this.token)
-                .request(Method.GET, "/api/v1/users/{userId}", userId)
-                .then();
-    }
-
-    public void 유저정보_수정_요청_With_File(final File file) {
-        this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, this.token)
-                .multiPart("file",
-                        file,
-                        MediaType.MULTIPART_FORM_DATA_VALUE)
-                .multiPart("request",
-                        mapToString(createUserUpdateRequestDto()),
-                        MediaType.APPLICATION_JSON_VALUE)
-                .request(Method.PUT, "/api/v1/users")
-                .then();
-    }
-
-    public void 유저정보_수정_요청_No_File() {
-        this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, this.token)
-                .multiPart("request",
-                        mapToString(createUserUpdateRequestDto()),
-                        MediaType.APPLICATION_JSON_VALUE)
-                .request(Method.PUT, "/api/v1/users")
-                .then();
-    }
-
-    public void 회원탈퇴_요청() {
-        this.response = given()
-                .header(HttpHeaders.AUTHORIZATION, this.token)
-                .request(Method.DELETE, "/api/v1/users")
-                .then();
     }
 }
