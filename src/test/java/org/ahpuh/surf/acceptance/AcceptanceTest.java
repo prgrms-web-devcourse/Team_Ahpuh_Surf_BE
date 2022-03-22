d4b71ec778d88800185a40ed69ef01bfb0cbbe2c
cse0518 <g787@naver.com>
2022-03-23 00:08:14 +0900

    refactor: BeforeEach 메소드명 변경

--- src/test/java/org/ahpuh/surf/acceptance/AcceptanceTest.java
+++ src/test/java/org/ahpuh/surf/acceptance/AcceptanceTest.java
@@ -2,9 +2,6 @@ package org.ahpuh.surf.acceptance;
 
 import io.restassured.RestAssured;
 import org.ahpuh.surf.config.DatabaseCleaner;
-import org.ahpuh.surf.jwt.Claims;
-import org.ahpuh.surf.jwt.Jwt;
-import org.ahpuh.surf.user.domain.Permission;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +13,14 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
     private DatabaseCleaner databaseCleaner;
 
     @BeforeEach
-    void setPort() {
+    void setRandomPort() {
         RestAssured.port = port;
     }
 
